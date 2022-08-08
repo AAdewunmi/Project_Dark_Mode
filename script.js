8f9bf1132516a65b4447748b8a4cd4296325b2f5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-08 21:09:02 +0100

    Thirty Seven Commit: Implement container property "classList" and classList method "dark"

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+/*jslint node: true */
+"use strict";
+
 const container = document.querySelector(".container");
 const toggle = document.querySelector(".toggle");
 
 toggle.addEventListener("click", () => {
-  
+  container.classList.toggle("dark");
 });
